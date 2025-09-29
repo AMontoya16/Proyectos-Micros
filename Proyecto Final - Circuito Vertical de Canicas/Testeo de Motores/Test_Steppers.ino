3cecbc7e7dde76f7132d57d8ed564fc0d758a204
AMontoya16 <andrew16032005@gmail.com>
2025-09-28 18:01:20 -0600

    Cambios con prueba a la base horizontal
    Testeo de variacion de parámetros de velocidad y aceleración para que la base horizontal funcione correctamente.

--- Proyecto Final - Circuito Vertical de Canicas/Testeo de Motores/Test_Steppers.ino
+++ Proyecto Final - Circuito Vertical de Canicas/Testeo de Motores/Test_Steppers.ino
@@ -15,18 +15,31 @@ AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 const int STEPS_PER_REV = 200;
 
 void setup() {
+<<<<<<< Updated upstream
   stepper.setMaxSpeed(1000);
   stepper.setAcceleration(500);
+=======
+  stepper.setMaxSpeed(700);
+  stepper.setAcceleration(1100);
+>>>>>>> Stashed changes
   stepper.setCurrentPosition(0);
 }
 
 void loop() {
+<<<<<<< Updated upstream
   stepper.moveTo(-4 * STEPS_PER_REV);
+=======
+  stepper.moveTo(-8 * STEPS_PER_REV);
+>>>>>>> Stashed changes
   stepper.runToPosition();
 
   stepper.setCurrentPosition(0);
 
+<<<<<<< Updated upstream
   stepper.moveTo(2 * STEPS_PER_REV);
+=======
+  stepper.moveTo(8 * STEPS_PER_REV);
+>>>>>>> Stashed changes
   stepper.runToPosition();
 
   stepper.setCurrentPosition(0);
